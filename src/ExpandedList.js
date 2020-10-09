a694fa49b89f2f90c26cffb62bccede5008e1143
Henry Hill <henry.jacob.hill@gmail.com>
2020-10-09 13:58:47 -0400

    added click to exit expanded list

--- src/ExpandedList.js
+++ src/ExpandedList.js
@@ -98,6 +98,17 @@ const ExpandedList = (props) => {
           </div>
         </div>
       </div>
+      <div
+        onClick={props.closeExpandedList}
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: "0",
+          left: "0",
+          zIndex: "-10",
+        }}
+      ></div>
     </div>
   );
 };
